741d03db16696e978ac0ad670ed4d594292c3178
Flow-Millot <florentin.millot@epfedu.fr>
2026-03-29 13:55:54 +0200

    Image compte google & deconnexion

--- app/urls.py
+++ app/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('register/', views.register_view, name='register'),
     path('logout/', views.logout_view, name='logout'),
     path('accounts/', include('allauth.urls')),
+    path('account/', views.account_view, name='account'),
     path('add-beer/', views.add_beer_view, name='add_beer'),
     path('beers/', views.all_beers_view, name='all_beers'),
     path('api/search-brewery/', views.search_brewery, name='search_brewery'),
