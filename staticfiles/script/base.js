cafe3a8430ab79fe2c300eec4f18573dfa05d136
Flow-Millot <florentin.millot@epfedu.fr>
2026-08-26 11:57:17 +0200

    bug fixes

--- staticfiles/script/base.js
+++ staticfiles/script/base.js
@@ -3,7 +3,7 @@ document.addEventListener('submit', function(event) {
     const submitBtn = form.querySelector('button[type="submit"], input[type="submit"]');
     
     if (submitBtn) {
-        submitBtn.disabled = true;
+        //submitBtn.disabled = true;
         submitBtn.style.opacity = '0.7';
     }
 });
